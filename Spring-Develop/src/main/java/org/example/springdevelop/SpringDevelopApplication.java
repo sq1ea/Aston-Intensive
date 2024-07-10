0898018d46786c61a6e212f1aacc147c6c07d716
sq1ea <quality.script@outlook.com>
2024-07-10 12:07:01 +0300

    init spring

---
+++ Spring-Develop/src/main/java/org/example/springdevelop/SpringDevelopApplication.java
@@ -0,0 +1,13 @@
+package org.example.springdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevelopApplication.class, args);
+	}
+
+}
---
+++ Spring-Develop/src/test/java/org/example/springdevelop/SpringDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
